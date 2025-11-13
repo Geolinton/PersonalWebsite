f9f7d682a0fd441994ec85064b87d85312a9fb82
Praise Idowu <101899604+Geolinton@users.noreply.github.com>
2025-11-13 15:55:42 -0600

    minor changes

--- homePage.py
+++ homePage.py
@@ -6,15 +6,15 @@ st.set_page_config(page_title=page_title, page_icon=":house_with_garden:",layout
 def homePage():
     st.title(page_title + " :house_with_garden:")
     st.markdown("""
-    Welcome to Praise's Home Page! This application is designed to help you train machine learning models and view detailed results of your experiments.
+        Welcome to Praise's Home Page! This application is designed to help you train machine learning models and view detailed results of your experiments.
 
-    ### Features:
-    - **Train Model**: Navigate to the training page to build and train your machine learning models.
-    - **Detailed Results**: View comprehensive results and analyses of your model training experiments.
-    - **About**: Learn more about this application and its purpose.
-    - **Contact**: Get in touch for support or inquiries.
+        ### Features:
+        - **Train Model**: Navigate to the training page to build and train your machine learning models.
+        - **Detailed Results**: View comprehensive results and analyses of your model training experiments.
+        - **About**: Learn more about this application and its purpose.
+        - **Contact**: Get in touch for support or inquiries.
 
-    Use the sidebar to navigate through different sections of the application.
+        Use the sidebar to navigate through different sections of the application.
     """)
 
 def main():
